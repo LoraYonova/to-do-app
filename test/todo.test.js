d0679999feccb188dfddacb75a99b782e1a47219
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2024-03-15 14:56:07 +0200

    user can delete a task

--- test/todo.test.js
+++ test/todo.test.js
@@ -8,3 +8,15 @@ test('user can add a task', async ({page}) =>{
     const taskText = await page.textContent('.task');
     expect(taskText).toContain('Test Task');
 })
+
+test('user can delete a task', async ({page}) => {
+    await page.goto('http://localhost:5500/');
+    await page.fill('#task-input', 'Test Task');
+    await page.click('#add-task');
+    await page.click('.task .delete-task');
+
+    const tasks = await page.$$eval('.task', 
+    tasks => tasks.map(task => task.textContent));
+    
+    expect(tasks).not.toContain('Test Task');
+})
